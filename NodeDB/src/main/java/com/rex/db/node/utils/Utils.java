2c418e112e870b70d02951eb35f10e27dc3c5fe9
Rex Luciano <64059802+rexllc@users.noreply.github.com>
2022-07-12 21:13:32 +0800

    Improved and added new method

--- NodeDB/src/main/java/com/rex/db/node/utils/Utils.java
+++ NodeDB/src/main/java/com/rex/db/node/utils/Utils.java
@@ -1,6 +1,13 @@
 package com.rex.db.node.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Map;
 
 public class Utils {
 
@@ -16,13 +23,44 @@ public class Utils {
 		return sb.toString();
 	}
 
-	public static int getIndexOf(ArrayList<Object> arrayList, String query) {
+	public static int getIndexOf(List<String> arrayList, String query) {
 		for (int i = 0; i < arrayList.size(); i++) {
 			if (arrayList.get(i).equals(query)) {
 				return i;
 			}
 		}
 		return -1;
+	}
+
+	public static Map<String, Object> toMap(JSONObject jsonobj) throws JSONException {
+		Map<String, Object> map = new HashMap<String, Object>();
+		Iterator<String> keys = jsonobj.keys();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			Object value = jsonobj.get(key);
+			if (value instanceof JSONArray) {
+				value = toList((JSONArray) value);
+			} else if (value instanceof JSONObject) {
+				value = toMap((JSONObject) value);
+			}
+			map.put(key, value);
+		}
+		return map;
+	}
+
+	public static List<Object> toList(JSONArray array) throws JSONException {
+		List<Object> list = new ArrayList<Object>();
+		for (int i = 0; i < array.length(); i++) {
+			Object value = array.get(i);
+			if (value instanceof JSONArray) {
+				value = toList((JSONArray) value);
+			} else if (value instanceof JSONObject) {
+				value = toMap((JSONObject) value);
+			}
+			list.add(value);
+		}
+		return list;
+
 	}
 
 }
